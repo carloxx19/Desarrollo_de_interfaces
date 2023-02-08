d21ad541a7c691ce9b113a7bf43a8dc5cac976b2
carloxx19 <carlosrtoledo.133@gmail.com>
2023-02-08 12:15:13 +0100

    react

---
+++ react/src/componentes/footer.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Footer(props) {
+    return (
+        <footer>
+            {props.pie}
+        </footer>
+    )
+}
+
+export default Footer;
